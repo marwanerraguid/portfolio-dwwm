df0f4b00713acc1535bd5b528b40b84a3f2464d2
Funukenichi <funukenichi@gmail.com>
2023-05-25 20:07:05 +0200

    local storage dark mode

--- script/burger.js
+++ script/burger.js
@@ -5,15 +5,15 @@ const menu = document.querySelector("menu");
 const toggleMode = document.querySelector(".toggle-mode");
 
 burger.addEventListener("click", () => {
-	menu.classList.toggle("active");
-	burger.classList.toggle("active");
-	toggleMode.classList.toggle("active");
+  menu.classList.toggle("active");
+  burger.classList.toggle("active");
+  toggleMode.classList.toggle("active");
 });
 
 document.querySelectorAll("li").forEach((n) =>
-	n.addEventListener("click", () => {
-		menu.classList.remove("active");
-		burger.classList.remove("active");
-		toggleMode.classList.remove("active");
-	})
+  n.addEventListener("click", () => {
+    menu.classList.remove("active");
+    burger.classList.remove("active");
+    toggleMode.classList.remove("active");
+  })
 );
--- script/mode.js
+++ script/mode.js
@@ -3,12 +3,29 @@
 const toggle = document.querySelector(".toggle-mode");
 
 toggleMode.addEventListener("click", () => {
-	console.log("La classe dark a bien été ajoutée au body");
-	document.body.classList.toggle("dark");
-	if (document.body.classList.contains("dark")) {
-		toggleMode.innerHTML = `<i class="fa-solid fa-sun"></i>`;
-	} else {
-		document.body.classList.remove("dark");
-		toggleMode.innerHTML = `<i class="fa-solid fa-moon"></i>`;
-	}
+  document.body.classList.toggle("dark");
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    toggleMode.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+  } else {
+    document.body.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+    toggleMode.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+  }
 });
+
+let getMode = localStorage.getItem("theme");
+
+if (getMode && getMode === "dark") {
+  document.body.classList.add("dark");
+}
+
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", ({ matches }) => {
+    if (document.body.classList.contains("dark")) {
+      document.body.classList.toggle("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  });
