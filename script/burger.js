243f936bcc56ce3d3961eabe12b4bacdaa8f1d42
Erraguid Marwan <funukenichi@gmail.com>
2023-05-25 13:24:55 +0200

    menu burger

--- script/burger.js
+++ script/burger.js
@@ -2,15 +2,18 @@
 
 const burger = document.querySelector(".burger");
 const menu = document.querySelector("menu");
+const toggleMode = document.querySelector(".toggle-mode");
 
 burger.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  burger.classList.toggle("active");
+	menu.classList.toggle("active");
+	burger.classList.toggle("active");
+	toggleMode.classList.toggle("active");
 });
 
 document.querySelectorAll("li").forEach((n) =>
-  n.addEventListener("click", () => {
-    menu.classList.remove("active");
-    burger.classList.remove("active");
-  })
+	n.addEventListener("click", () => {
+		menu.classList.remove("active");
+		burger.classList.remove("active");
+		toggleMode.classList.remove("active");
+	})
 );
